9c30b6df9584a018d35f4cb634a06831f5e290ac
scottmac-dev <scott.macdonald2023@outlook.com>
2025-07-30 12:16:06 +0000

    Correct implementation and unit tests for remove

--- src/structures/skip_list.rs
+++ src/structures/skip_list.rs
@@ -136,27 +136,36 @@ impl<T: Ord + Clone + Debug> SkipList<T> {
     }
     /// Remove a value from the skip list
     pub fn remove(&mut self, target: T) {
-        let mut prev = self.search(&target);
-        let node_to_remove = prev[0].borrow().links[0];
+        let prev = self.search(&target);
 
-        if let Some(node) = node_to_remove {
-            if node.borrow().data.as_ref() != Some(&target) {
-                println!("Target {:?}, not found in SkipList", &target);
+        // Try to find node after prev[0] level [0]
+        let node_to_remove = prev[0].borrow().links[0].clone();
+
+        if let Some(node_rc) = node_to_remove {
+            // Validate node matches target, exit if no match
+            if node_rc.borrow().data.as_ref() != Some(&target) {
+                println!("Target {:?} not found in SkipList", &target);
                 return;
             }
-        }
-
-        for i in 0..prev.len() {
-            if let Some(node) = node_to_remove {
-                let next = node.borrow().links.get(i).cloned().unwrap_or(None);
-                prev[i].borrow_mut().links[i] = Some(Rc::clone(&next));
+            // Update all levels where node exists to point forward or to None
+            for level in 0..prev.len() {
+                // Get the next pointer of removed node
+                let next = node_rc
+                    .borrow()
+                    .links
+                    .get(level) // current index level
+                    .cloned() // clone if exists
+                    .unwrap_or(None); // None if index out of bounds
+                // Update prev to skip over removed node
+                prev[level].borrow_mut().links[level] = next.clone();
             }
+            self.size -= 1; // decrement list size
+        } else {
+            // Output if node_to_remove returned None
+            println!("Target {:?} not found in SkipList", &target);
         }
-
-        self.size -= 1;
     }
 }
-
 // Unit testing module
 #[cfg(test)]
 mod tests {
@@ -206,5 +215,21 @@ mod tests {
         let not_found_value = test_list.contains(99);
         assert_eq!(found_value, true);
         assert_eq!(not_found_value, false);
+    }
+    #[test]
+    fn test_remove() {
+        // test list of usize type
+        let mut test_list: SkipList<usize> = SkipList::new();
+        test_list.insert(3);
+        test_list.insert(5);
+        test_list.insert(10);
+        test_list.insert(1);
+        let found_value = test_list.contains(10);
+        assert_eq!(found_value, true);
+        test_list.remove(10);
+        let removed = test_list.find(10);
+        let removed2 = test_list.contains(10);
+        assert_eq!(removed, None);
+        assert_eq!(removed2, false);
     }
 }
